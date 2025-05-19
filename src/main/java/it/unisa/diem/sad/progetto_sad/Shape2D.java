44c496c9d09b4b82bcbb713f62a751226f5ac875
Marty3ja <m.treglia3@studenti.unisa.it>
2025-05-19 16:27:14 +0200

    ShapeInterface

---
+++ src/main/java/it/unisa/diem/sad/progetto_sad/Shape2D.java
@@ -0,0 +1,17 @@
+package it.unisa.diem.sad.progetto_sad;
+import javafx.scene.paint.Paint;
+
+public interface Shape2D extends ShapeInterface{
+    void setX(double X);
+    void setY( double Y);
+    double getX();
+    double getY();
+    void setStrokeColor(Paint color);
+    Paint getStrokeColor();
+    void setWidth( double width);
+    double getWidth();
+    void setHeigth( double heigth);
+    double getHeigth();
+    void setFillColor( Paint color);
+    Paint getFillColor();
+}
