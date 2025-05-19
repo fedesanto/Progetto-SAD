1e927b1b9d3b1967e20559e53e0f7527c3fcdffb
Marty3ja <m.treglia3@studenti.unisa.it>
2025-05-19 16:13:43 +0200

    ShapeInterface

---
+++ src/main/java/it/unisa/diem/sad/progetto_sad/ShapeInterface.java
@@ -0,0 +1,12 @@
+package it.unisa.diem.sad.progetto_sad;
+import javafx.scene.paint.Paint;
+
+public interface ShapeInterface {
+    void setX(double X);
+    void setY( double Y);
+    double getX();
+    double getY();
+    void setStrokeColor(Paint color);
+    Paint getStrokeColor();
+
+}
