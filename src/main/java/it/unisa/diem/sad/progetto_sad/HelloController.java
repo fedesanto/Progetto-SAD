1a5db59e3f014c231886753e3ae38f5e0494be0d
asessa1 <a.sessa111@studenti.unisa.it>
2025-05-19 17:01:41 +0200

    Implementata funzionalità di aggiunta forme

--- src/main/java/it/unisa/diem/sad/progetto_sad/HelloController.java
+++ src/main/java/it/unisa/diem/sad/progetto_sad/HelloController.java
@@ -2,29 +2,39 @@ package it.unisa.diem.sad.progetto_sad;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ScrollPane;
 
 public class HelloController {
-    public enum TYPE { LINE, RECTANGLE, ELLIPSE }
 
     @FXML
     private ColorPicker strokeColorPicker;
     @FXML
     private ColorPicker fillColorPicker;
+    @FXML
+    private ScrollPane pane;
 
     private ShapeCreator selectedShape;
 
     @FXML
     protected void chosenLine() {
-        selectedShape = new Shape1DCreator(TYPE.LINE, strokeColorPicker.getValue());
+        selectedShape = new Shape1DCreator(Shape1D.TYPE_1D.LINE, strokeColorPicker.getValue());
     }
 
     @FXML
     protected void chosenRectangle() {
-        selectedShape = new Shape2DCreator(TYPE.RECTANGLE, strokeColorPicker.getValue(), fillColorPicker.getValue());
+        selectedShape = new Shape2DCreator(Shape2D.TYPE_2D.RECTANGLE, strokeColorPicker.getValue(), fillColorPicker.getValue());
     }
 
     @FXML
     protected void chosenEllipse() {
-        selectedShape = new Shape2DCreator(TYPE.ELLIPSE, strokeColorPicker.getValue(), fillColorPicker.getValue());
+        selectedShape = new Shape2DCreator(Shape2D.TYPE_2D.ELLIPSE, strokeColorPicker.getValue(), fillColorPicker.getValue());
     }
+
+    @FXML
+    protected void addShape() {
+        ShapeInterface shape = selectedShape.createShape();
+        pane.getChildren().add(shape.getShape());
+    }
+
+
 }
