7cd5bb0eb1372f567c6b5302dfbda3bc76e661cd
asessa1 <a.sessa111@studenti.unisa.it>
2025-05-19 16:35:26 +0200

    Impostata la modalità di disegno

--- src/main/java/it/unisa/diem/sad/progetto_sad/HelloController.java
+++ src/main/java/it/unisa/diem/sad/progetto_sad/HelloController.java
@@ -1,14 +1,30 @@
 package it.unisa.diem.sad.progetto_sad;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ColorPicker;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public enum TYPE { LINE, RECTANGLE, ELLIPSE }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Io sono Attilio");
+    private ColorPicker strokeColorPicker;
+    @FXML
+    private ColorPicker fillColorPicker;
+
+    private ShapeCreator selectedShape;
+
+    @FXML
+    protected void chosenLine() {
+        selectedShape = new Shape1DCreator(TYPE.LINE, strokeColorPicker.getValue());
+    }
+
+    @FXML
+    protected void chosenRectangle() {
+        selectedShape = new Shape2DCreator(TYPE.RECTANGLE, strokeColorPicker.getValue(), fillColorPicker.getValue());
+    }
+
+    @FXML
+    protected void chosenEllipse() {
+        selectedShape = new Shape2DCreator(TYPE.ELLIPSE, strokeColorPicker.getValue(), fillColorPicker.getValue());
     }
 }
