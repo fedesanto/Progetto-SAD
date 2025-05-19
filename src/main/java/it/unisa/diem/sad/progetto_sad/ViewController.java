4aff8b0471fe33137b435dcf4c025b96421ae9e9
feder <f.santonicola10@studenti.unisa.it>
2025-05-19 17:15:33 +0200

    Aggiunti i pulsanti di selezione delle forme

--- src/main/java/it/unisa/diem/sad/progetto_sad/ViewController.java
+++ src/main/java/it/unisa/diem/sad/progetto_sad/ViewController.java
@@ -1,11 +1,43 @@
 package it.unisa.diem.sad.progetto_sad;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
 
-public class ViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewController implements Initializable {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Pane selectableShapes;
+    private Shape highlightedShape;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    protected void onHighlighting() {
+        DropShadow highlight = new DropShadow(20, Color.BLUE);
+
+        for (Node shape : selectableShapes.getChildren()) {
+            if (highlightedShape != null) {
+                highlightedShape.setEffect(null);
+            }
+            highlightedShape = (Shape) shape;
+            highlightedShape.setEffect(highlight);
+        }
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
