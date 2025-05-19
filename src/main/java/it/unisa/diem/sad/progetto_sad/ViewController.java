2c27d459eb766e498b2c7399ed4546790a9c87f1
feder <f.santonicola10@studenti.unisa.it>
2025-05-19 16:08:13 +0200

    Creata interfaccia grafica

--- src/main/java/it/unisa/diem/sad/progetto_sad/ViewController.java
+++ src/main/java/it/unisa/diem/sad/progetto_sad/ViewController.java
@@ -3,12 +3,12 @@ package it.unisa.diem.sad.progetto_sad;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ViewController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Io sono Attilio");
+
     }
 }
