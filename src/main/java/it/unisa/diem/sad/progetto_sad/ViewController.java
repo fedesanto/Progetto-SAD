2398b6c8f29145ec38a3ddd38fac8cc9270b1fa9
asessa1 <a.sessa111@studenti.unisa.it>
2025-05-20 10:46:30 +0200

    Aggiunti i metodi di gestione salvataggio file e caricamento file in FileManager. Implemenati gli eventi di gestione delle operazioni sui file nel Controller

--- src/main/java/it/unisa/diem/sad/progetto_sad/ViewController.java
+++ src/main/java/it/unisa/diem/sad/progetto_sad/ViewController.java
@@ -3,9 +3,11 @@ package it.unisa.diem.sad.progetto_sad;
 import it.unisa.diem.sad.progetto_sad.factories.Shape1DCreator;
 import it.unisa.diem.sad.progetto_sad.factories.Shape2DCreator;
 import it.unisa.diem.sad.progetto_sad.factories.ShapeCreator;
+import it.unisa.diem.sad.progetto_sad.fileHandlers.FileManager;
 import it.unisa.diem.sad.progetto_sad.shapes.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseButton;
@@ -13,7 +15,12 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Shape;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -30,7 +37,7 @@ public class ViewController implements Initializable {
     private Pane workspace;
 
     private Shape highlightedShape;
-    private ShapeCreator selectedShape;
+    private ShapeCreator chosenShape;
 
     /**
      * Inizializza il controller dopo il caricamento del file FXML.
@@ -46,15 +53,15 @@ public class ViewController implements Initializable {
     /**
      * Evidenzia la forma selezionata
      *
-     * @param selectedShape riferimento alla shape cliccata
+     * @param chosenShape riferimento alla shape cliccata
      */
-    private void highlightShape(Shape selectedShape) {
+    private void highlightShape(Shape chosenShape) {
         DropShadow highlight = new DropShadow(20, Color.BLUE);  // effetto di evidenziazione
 
         if (highlightedShape != null) {         // disattivo l'effetto alla forma attualmente evidenziata
             highlightedShape.setEffect(null);
         }
-        highlightedShape = selectedShape;       // evidenzio la forma cliccata
+        highlightedShape = chosenShape;       // evidenzio la forma cliccata
         highlightedShape.setEffect(highlight);
     }
 
@@ -66,7 +73,7 @@ public class ViewController implements Initializable {
      */
     @FXML
     protected void chosenLine(MouseEvent e) {
-        selectedShape = new Shape1DCreator(Shape1D.TYPE_1D.LINE, strokeColorPicker.getValue());
+        chosenShape = new Shape1DCreator(Shape1D.TYPE_1D.LINE, strokeColorPicker.getValue());
 
         highlightShape((Shape) e.getTarget());
     }
@@ -79,7 +86,7 @@ public class ViewController implements Initializable {
      */
     @FXML
     protected void chosenRectangle(MouseEvent e) {
-        selectedShape = new Shape2DCreator(Shape2D.TYPE_2D.RECTANGLE, strokeColorPicker.getValue(), fillColorPicker.getValue());
+        chosenShape = new Shape2DCreator(Shape2D.TYPE_2D.RECTANGLE, strokeColorPicker.getValue(), fillColorPicker.getValue());
 
         highlightShape((Shape) e.getTarget());
     }
@@ -92,7 +99,7 @@ public class ViewController implements Initializable {
      */
     @FXML
     protected void chosenEllipse(MouseEvent e) {
-        selectedShape = new Shape2DCreator(Shape2D.TYPE_2D.ELLIPSE, strokeColorPicker.getValue(), fillColorPicker.getValue());
+        chosenShape = new Shape2DCreator(Shape2D.TYPE_2D.ELLIPSE, strokeColorPicker.getValue(), fillColorPicker.getValue());
 
         highlightShape((Shape) e.getTarget());
     }
@@ -102,9 +109,9 @@ public class ViewController implements Initializable {
      */
     @FXML
     protected void addShape(MouseEvent event) {
-        if(selectedShape != null) {
+        if(chosenShape != null) {
             if (event.getButton() == MouseButton.PRIMARY) {
-                ShapeInterface shape = selectedShape.createShape();
+                ShapeInterface shape = chosenShape.createShape();
                 shape.setCenterX(event.getX());
                 shape.setCenterY(event.getY());
                 workspace.getChildren().add((Shape) shape);
@@ -117,7 +124,7 @@ public class ViewController implements Initializable {
      */
     @FXML
     protected void pickedStrokeColor() {
-        selectedShape.setStrokeColor(strokeColorPicker.getValue());
+        chosenShape.setStrokeColor(strokeColorPicker.getValue());
     }
 
     /**
@@ -125,8 +132,45 @@ public class ViewController implements Initializable {
      */
     @FXML
     protected void pickedFillColor() {
-        if(selectedShape instanceof Shape2DCreator) {
-            ((Shape2DCreator) selectedShape).setFillColor(fillColorPicker.getValue());
+        if(chosenShape instanceof Shape2DCreator) {
+            ((Shape2DCreator) chosenShape).setFillColor(fillColorPicker.getValue());
+        }
+    }
+
+    @FXML
+    protected void saveFileOperation() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files (.txt)", ".txt"));
+        File file = fileChooser.showSaveDialog(null);
+
+        if(file.getName().endsWith(".txt")){
+            List<ShapeInterface> shapeList = new ArrayList<>();
+
+            for (Node node : workspace.getChildren())
+                shapeList.add((ShapeInterface) node);
+
+            FileManager.saveFile(shapeList, file.getAbsolutePath());
+        }
+        else {
+            System.out.println("File non valido");
+        }
+    }
+
+    @FXML
+    protected void loadFileOperation() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files (.txt)", ".txt"));
+        File file = fileChooser.showOpenDialog(null);
+
+        if(file.getName().endsWith(".txt")){
+            List<ShapeInterface> shapeList = FileManager.loadFile(file.getAbsolutePath());
+
+            workspace.getChildren().clear();
+            for (ShapeInterface shape : shapeList)
+                workspace.getChildren().add((Shape) shape);
+        }
+        else {
+            System.out.println("File non valido");
         }
     }
 }
--- src/main/java/it/unisa/diem/sad/progetto_sad/fileHandlers/FileManager.java
+++ src/main/java/it/unisa/diem/sad/progetto_sad/fileHandlers/FileManager.java
@@ -1,23 +1,101 @@
 package it.unisa.diem.sad.progetto_sad.fileHandlers;
 
-import it.unisa.diem.sad.progetto_sad.shapes.ShapeInterface;
+import it.unisa.diem.sad.progetto_sad.factories.Shape1DCreator;
+import it.unisa.diem.sad.progetto_sad.factories.Shape2DCreator;
+import it.unisa.diem.sad.progetto_sad.shapes.*;
+import javafx.scene.paint.Color;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
 
-    public static boolean saveFile(List<ShapeInterface> shapes, String path) {
-        try (FileWriter writer = new FileWriter(path)) {
+    public static boolean saveFile(List<ShapeInterface> shapes, String file) {
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file)))) {
             for (ShapeInterface shape : shapes) {
-                writer.write(shape.toString() + "\n");
+                writer.println(shape.toString());
             }
-            return true;
         } catch (IOException e) {
-            e.printStackTrace();
             return false;
         }
+        return true;
     }
+
+    public static List<ShapeInterface> loadFile(String file) {
+        List<ShapeInterface> shapes = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String linea;
+
+            while ((linea = reader.readLine()) != null) {
+                String[] campi = linea.split(";");
+
+                if(campi[0].equals("Shape1D")) {
+                    if(campi[1].equals("SegmentShape")) {
+                        double centerX = Double.parseDouble(campi[2]);
+                        double centerY = Double.parseDouble(campi[3]);
+                        Color stroke = Color.web(campi[4]);
+                        double length = Double.parseDouble(campi[5]);
+
+                        Shape1DCreator creator = new Shape1DCreator(Shape1D.TYPE_1D.LINE, stroke);
+                        SegmentShape shape = (SegmentShape) creator.createShape();
+
+                        shape.setCenterX(centerX);
+                        shape.setCenterY(centerY);
+                        shape.setShapeLength(length);
+
+                        shapes.add(shape);
+                    }
+                }
+
+                if(campi[0].equals("Shape2D")){
+                    if(campi[1].equals("RectangleShape")) {
+                        double centerX = Double.parseDouble(campi[2]);
+                        double centerY = Double.parseDouble(campi[3]);
+                        Color stroke = Color.web(campi[4]);
+                        double width = Double.parseDouble(campi[5]);
+                        double height = Double.parseDouble(campi[6]);
+                        Color fill = Color.web(campi[7]);
+
+                        Shape2DCreator creator = new Shape2DCreator(Shape2D.TYPE_2D.RECTANGLE, stroke, fill);
+                        RectangleShape shape = (RectangleShape) creator.createShape();
+
+                        shape.setCenterX(centerX);
+                        shape.setCenterY(centerY);
+                        shape.setShapeWidth(width);
+                        shape.setShapeHeight(height);
+
+                        shapes.add(shape);
+                    }
+
+                    if(campi[1].equals("Ellipse")) {
+                        double centerX = Double.parseDouble(campi[2]);
+                        double centerY = Double.parseDouble(campi[3]);
+                        Color stroke = Color.web(campi[4]);
+                        double width = Double.parseDouble(campi[5]);
+                        double height = Double.parseDouble(campi[6]);
+                        Color fill = Color.web(campi[7]);
+
+                        Shape2DCreator creator = new Shape2DCreator(Shape2D.TYPE_2D.ELLIPSE, stroke, fill);
+                        EllipseShape shape = (EllipseShape) creator.createShape();
+
+                        shape.setCenterX(centerX);
+                        shape.setCenterY(centerY);
+                        shape.setShapeWidth(width);
+                        shape.setShapeHeight(height);
+
+                        shapes.add(shape);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            return null;
+        }
+
+        return shapes;
+
+    }
+
 }
 
